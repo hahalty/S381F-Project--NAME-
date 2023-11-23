c58e0bd5b79cb6edc69d4d491c5757226c3559b7
hahalty <lautszyin1111@gmail.com>
2023-11-23 22:02:53 +0800

    last push

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
     const assert = require('assert');
+    const mongoose  = require('mongoose');
+    const UserSchma = new mongoose.Schema({
+            name : String,
+            quantity : Number,
+            price :Number})
+    const UserModel =mongoose.model("text", UserSchma)
 
     
+    
 
+    const MongoClient = require('mongodb').MongoClient;
+    const ObjectID = require('mongodb').ObjectID;
+
+    const mongourl = 'mongodb+srv://admin:admin@cluster0.chetaf4.mongodb.net/'; 
+    const dbName = 'test';
 
 
     const express = require('express');
@@ -12,12 +24,61 @@
 
     var usersinfo = new Array(
         {name: "admin", password: "admin"},
-        {name: "student", password: "student"},
-        {name: "student2", password: "student2"}
+        {name: "ac", password: "pw"},
+        {name: "worker", password: "worker"}
     );
 
     var documents = {};
     //Main Body
+
+
+
+    async function connect(){
+        try{
+            await   mongoose.connect(mongourl);
+            console.log("Connected to MongoDB");
+
+        }catch(error){
+            console.error(error);
+
+        }
+        }
+
+
+    connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     app.use(express.static('public'));
     
     
@@ -59,6 +120,12 @@
             req.session.userid = usersinfo[i].name;
             console.log(req.session.userid);
             return res.status(200).redirect("/home");
+            }else {
+
+                
+
+
+
             }
         }
             console.log("Error username or password.");
@@ -75,10 +142,44 @@
         console.log("...Welcome to the home page!");
         return res.status(200).render("home");
     });
+    
+    app.get('/create', function(req, res){
+        console.log("...Welcome to the create page!");
+        return res.status(200).render("create");
+    });
+    
+    app.get('/read', function(req, res){
+        console.log("...Welcome to the read page!");
+        const collection = db.test.find();
+
+
+    });
+    
+   
+    app.get('/update', function(req, res){
+        console.log("...Welcome to the update page!");
+        return res.status(200).render("update");
+    });
+    
+    app.get('/delete', function(req, res){
+        console.log("...Welcome to the delete page!");
+        return res.status(200).render("delete");
+    });
+  
+
+    app.get('/read',(req ,res) =>{
+        UserModel.find()
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
 
 
 
 
+    }
+    
+    
+    
+    )
 
 
-    app.listen(app.listen(process.env.PORT || 5000));
+    app.listen(app.listen(process.env.PORT || 5000));   
